f9c920e5981629b61ccb1fd779e506dc49e732f7
Vasilev,Mihail M <m.vasilev@student.fontys.nl>
2024-03-11 18:50:43 +0100

    Test change for CICD

--- heardit/src/main/java/com/example/heardit/service/MusicService.java
+++ heardit/src/main/java/com/example/heardit/service/MusicService.java
@@ -31,7 +31,6 @@ public class MusicService {
 //        this.filePath = Path.of(filePath);
 //    }
     public List<String> getAvailableSongs() {
-        // Example: Assuming song files are named as "song1.mp3", "song2.mp3", etc.
         return Arrays.asList("Nine Lives - Unicorn Heads", "Don't Fret - Quincas Moreira", "song3", "song4", "song5", "song6", "song7", "song8", "song9", "song10");
     }
 }
