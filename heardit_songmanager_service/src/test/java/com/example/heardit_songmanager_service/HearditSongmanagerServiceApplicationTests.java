1dbb26be4ddac9fa4ea7f8612a31b4bdc3390fb1
Vasilev,Mihail M <m.vasilev@student.fontys.nl>
2024-04-21 15:31:22 +0200

    song manager service initial commit

---
+++ heardit_songmanager_service/src/test/java/com/example/heardit_songmanager_service/HearditSongmanagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heardit_songmanager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearditSongmanagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
