b26daa32c95aafaa0527e02afff19357e2600ec7
Vasilev,Mihail M <m.vasilev@student.fontys.nl>
2024-05-04 14:19:49 +0200

    Search service initial setup

---
+++ heardit_search_service/src/test/java/com/example/heardit_search_service/HearditSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heardit_search_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearditSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
