313edd0b4829d4f76c13c6f10d247c1592b15a8a
Vasilev,Mihail M <m.vasilev@student.fontys.nl>
2024-06-02 18:15:50 +0200

    RabbitMQ service created

--- rabbitMQ_service/src/main/java/com/example/rabbitmq_service/service/RabbitMQSender.java
+++ rabbitMQ_service/src/main/java/com/example/rabbitmq_service/service/RabbitMQSender.java
@@ -1,4 +1,4 @@
-package com.example.heardit_songmanager_service.rabbitmq;
+package com.example.rabbitmq_service.service;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
