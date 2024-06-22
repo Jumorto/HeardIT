5a52cadbd0902b7fcf7adf1c57443bee63becfbd
Mihail Vasilev <mdvasilev22@gmail.com>
2024-06-22 14:25:49 +0200

    Updates to songmanager service

--- heardit_songmanager_service/src/main/java/com/example/heardit_songmanager_service/service/use_cases/GoogleCloudStorageServiceUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.example.heardit_songmanager_service.service.use_cases;
-
-import com.example.heardit_songmanager_service.domain.GetGoogleCloudStorageResponse;
-
-public interface GoogleCloudStorageServiceUseCase {
-    GetGoogleCloudStorageResponse generateSignedUrl(String bucketName, String objectName);
-}
